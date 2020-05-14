04f9221a24d2ec39187d8f87dceccb14ee10d578
chisoftltd <chisoftltd@gmail.com>
2020-05-14 03:12:13 +0200

    more commits

--- Startup.cs
+++ Startup.cs
@@ -6,34 +6,43 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace mywebchisoft
 {
     public class Startup
     {
+        
+        public IConfiguration Configuration {get; set;}
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddOptions();
             //services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             //app.UseMvc();
             //app.UseRouting();
-
-        app.UseStaticFiles();
+        app.UseMvc();
+        //app.UseStaticFiles();
         app.UseRouting();
-        app.UseCors();
+        //app.UseCors();
 
         //app.UseEndpoints(endpoints =>
         //{
